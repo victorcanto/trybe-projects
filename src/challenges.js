a8f23c1ae96e302bcf1c4fb3c725787938321471
Victor Canto <iamvictorcanto@gmail.com>
2021-03-02 16:20:54 -0300

    starting project, desafio 1, 2, 3 e 4 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -1,23 +1,30 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
+// Desafio 1 - OK
+function compareTrue(boo1, boo2) {
+  if (boo1 === true && boo2 === true) {
+    return true;
+  }
+  if (boo1 === false && boo2 === false) {
+    return false;
+  }
+  return false;
+}
+// Desafio 2 - OK
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+// Desafio 3 - Ok
+function splitSentence(str) {
+  return str.split(' ');
 }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+// Desafio 4 - OK
+function concatName(arr) {
+  let firstItem = arr[0];
+  let lastItem = arr[arr.length - 1];
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+  return `${lastItem}, ${firstItem}`;
 }
-
 // Desafio 5
 function footballPoints() {
   // seu código aqui
@@ -39,22 +46,10 @@ function fizzBuzz() {
 }
 
 // Desafio 9
+// eslint-disable-next-line no-unused-vars
 function encode() {
   // seu código aqui
 }
 function decode() {
   // seu código aqui
 }
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
