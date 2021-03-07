1994b93ab061d2d38af535041f20943232047ffe
Victor Canto <iamvictorcanto@gmail.com>
2021-03-07 01:36:47 -0300

    Finaliza desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrTechList, name) {
+  if (arrTechList.length === 0) {
+    return 'Vazio!';
+  }
+  let listOfTechs = [];
+  arrTechList.sort();
+
+  for (let index in arrTechList) {
+    listOfTechs.push({
+      tech: arrTechList[index],
+      name,
+    });
+  }
+  console.log(listOfTechs);
 }
+// Desafio 10 - Teste
+techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas');
 
 // Desafio 11
 function generatePhoneNumber() {
