00adf1ba2118b21bb04e691d3188886086caf421
Victor Canto <iamvictorcanto@gmail.com>
2021-03-09 12:17:45 -0300

    Finaliza desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,10 +18,32 @@ function techList(arrTechList, name) {
 console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrNumbers) {
+  let phoneNumber = '';
+  if (arrNumbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrNumbers.length; index += 1) {
+    let repeatCounter = 0;
+    for (
+      let indexRepeatCounter = 0;
+      indexRepeatCounter < arrNumbers.length;
+      indexRepeatCounter += 1
+    ) {
+      if (arrNumbers[index] === arrNumbers[indexRepeatCounter]) {
+        repeatCounter += 1;
+      }
+      if (arrNumbers[indexRepeatCounter] < 0 || arrNumbers[indexRepeatCounter] > 9 || repeatCounter >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      } else {
+        phoneNumber = `(${arrNumbers[0]}${arrNumbers[1]}) ${arrNumbers[2]}${arrNumbers[3]}${arrNumbers[4]}${arrNumbers[5]}${arrNumbers[6]}-${arrNumbers[7]}${arrNumbers[8]}${arrNumbers[9]}${arrNumbers[10]}`;
+      }
+    }
+  }
+  return phoneNumber;
 }
-
+// Desafio 11 - Teste
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
