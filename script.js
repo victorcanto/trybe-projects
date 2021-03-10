9d5d35fe394bdf54f3690c050076647e2ea734a9
Victor Canto <iamvictorcanto@gmail.com>
2021-03-10 00:13:31 -0300

    Requesito 7 concluido

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.onload = function start() {
   const defaultColor = document.querySelector('.black');
   defaultColor.classList.add('selected');
 };
+const colorsPalette = document.querySelectorAll('.color');
+// Resolvi o problema usando essa ref: https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
+function selectColor() {
+  colorsPalette.forEach((color) => {
+    color.addEventListener('click', (_event) => {
+      colorsPalette[0].classList.remove('selected');
+      colorsPalette[1].classList.remove('selected');
+      colorsPalette[2].classList.remove('selected');
+      colorsPalette[3].classList.remove('selected');
+      color.classList.add('selected');
+    });
+  });
+}
+selectColor();
