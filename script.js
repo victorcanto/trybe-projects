279be8a84b2f5ec64d68a180c387e370c8eba94d
Eduardo Seije <eduardoseije@gmail.com>
2021-03-22 18:10:39 -0300

    Requisito 19 - teste 3

--- script.js
+++ script.js
@@ -10,23 +10,21 @@ buttonLogin.addEventListener('click', alertValueLogin);
 function validateForm() {
   const input = document.querySelectorAll('.right-content input');
   for (let index = 0; index < input.length; index += 1) {
-    let inputsForm = input[index].value;
+    const inputsForm = input[index].value;
     if (inputsForm === '') {
-      let validate = document.querySelector('.new-account-form');
-      let message = document.createElement('div');
-      message.innerHTML = input[index].name + 'Campos inválidos';
+      const validate = document.querySelector('.new-account-form');
+      const message = document.createElement('div');
+      message.innerHTML = `${input[index].name}Campos inválidos`;
       validate.appendChild(message);
     }
   }
 }
-document
-  .querySelector('#facebook-register')
-  .addEventListener('click', validateForm);
+document.querySelector('#facebook-register').addEventListener('click', validateForm);
 
 function gender() {
   const genderOther = document.querySelector('.other');
-  genderOther.addEventListener('click', function () {
-    let newInput = document.createElement('input');
+  genderOther.addEventListener('click', function createInput() {
+    const newInput = document.createElement('input');
     newInput.type = 'text';
     newInput.name = 'gender-custom';
     newInput.placeholder = 'Gênero';
