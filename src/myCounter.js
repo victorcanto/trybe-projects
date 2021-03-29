ce59485556706db98ba715ac21d10612eadbeaca
Victor Canto <iamvictorcanto@gmail.com>
2021-03-29 17:33:51 -0300

    #myCounter finalizado

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (var counterA = 0; counterA <= 3; counterA += 1) {
+    myArray.push(counterA);
+    for (var counterB = 2; counterB <= 3; counterB += 1) {
+      myArray.push(counterB);
     }
   }
   return myArray;
