f349ec0fc24776eabbb1bf99f22dd10abe44f78e
Victor Canto <iamvictorcanto@gmail.com>
2021-03-30 20:27:51 -0300

    #vqv finalizado

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const msg = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  if (!nome && !idade) {
+    return undefined;
+  }
+  return msg;
+};
 
 module.exports = vqv;
