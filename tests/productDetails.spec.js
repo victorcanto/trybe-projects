813e299ffaca23b548351cfb25df2afc1d056293
Victor Canto <iamvictorcanto@gmail.com>
2021-03-30 22:44:55 -0300

    Correcoes ESLint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require("assert");
-const productDetails = require("../src/productDetails");
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -31,14 +31,14 @@ const productDetails = require("../src/productDetails");
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe("#productDetails", () => {
+describe('#productDetails', () => {
   const p1 = productDetails()[0].name;
   const p2 = productDetails()[1].name;
-  it("tests the function has the correct behaviour", () => {
+  it('tests the function has the correct behaviour', () => {
     assert.strictEqual(typeof productDetails(), 'object');
     assert.strictEqual(productDetails().length, 2);
     assert.strictEqual(typeof productDetails()[0] === 'object' && typeof productDetails()[1] === 'object', true);
     assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
-    assert.strictEqual(productDetails(p1, p2)[0].details.productId === `${p1}123`  && productDetails()[1].details.productId === `${p2}123`, true);
+    assert.strictEqual(productDetails(p1, p2)[0].details.productId === `${p1}123` && productDetails()[1].details.productId === `${p2}123`, true);
   });
 });
