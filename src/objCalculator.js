29e4e887fb3e1a3d47e5cbab9f525c6c3c09b1ed
Victor Canto <iamvictorcanto@gmail.com>
2021-03-30 22:40:45 -0300

    #objCalculator refatorado

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -1,16 +1,8 @@
 const calculator = {
-  add: (int1, int2) => {
-    return int1 + int2;
-  },
-  mult: (int1, int2) => {
-    return int1 * int2;
-  },
-  div: (int1, int2) => {
-    return Math.floor(int1 / int2);
-  },
-  sub: (int1, int2) => {
-    return int1 - int2;
-  },
+  add: (int1, int2) => int1 + int2,
+  mult: (int1, int2) => int1 * int2,
+  div: (int1, int2) => Math.floor(int1 / int2),
+  sub: (int1, int2) => int1 - int2,
 };
 
 module.exports = calculator;
