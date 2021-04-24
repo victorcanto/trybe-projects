418966d2b49dfe0efda4fb91f4e90ff88df4f89c
Victor Canto <iamvictorcanto@gmail.com>
2021-04-24 13:32:48 -0300

    remove default comments from the project

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,27 +1,5 @@
 const api = require('../src/mockApi');
 
-/*
-A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
-No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
-
-Faça um mock da função fetchURL() de forma que,
-independa de chamadas de API e retorne as seguintes informações do Tunico:
-- Gênero: Masculino
-- Primeiro nome: Antônio
-- Último nome: Britto
-- País: Brasil
-- Email: tunico@bol.com.br (Sim, é um email do bol mesmo...)
-- Nome de usuário: tunicao123
-- Senha: 1234567890 (Usem senhas fortes, crianças!)
-
-Note que as informações devem estar de acordo com o JSON
-presente no README.md do projeto.
-
-Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
-
 const myUser = {
   gender: 'male',
   name: { first: 'Antônio', last: 'Britto' },
--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -2,20 +2,6 @@ const mockFunctions = require('../src/mockFunctions');
 
 jest.mock('../src/mockFunctions');
 
-/*
-Criamos uma série de funções com eficiência duvidosa.
-Elas estão no arquivo 'src/mockFunctions.js'.
-Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
-não como estão sendo feitos no arquivo original.
-A idéia é que os novos testes sobrescrevam os testes
-importados apenas na suite de testes abaixo.
-
-Importante! A correção de código via mock functions não é uma aplicação usual.
-O foco aqui é a utilização de mock functions.
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
-
 describe('verifica as funções e os mocks', () => {
 // source: https://jestjs.io/docs/mock-functions#mock-implementations
 // source: http://devfuria.com.br/logica-de-programacao/recursividade-fatorial/
