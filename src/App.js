d84b7075e39cf30d0802af93bf6b697463d9f404
Victor Canto <iamvictorcanto@gmail.com>
2021-04-28 19:01:13 -0300

    Requirement 3, 4 and 5 implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const result = movies
+      .map((movie) => <MovieCard key={ movie.title } info={ movie } />);
+    return (
+      <section>
+        {result}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
