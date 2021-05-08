a9cae7c491a89a8b7a935312eb8499c1add1ecee
Victor Canto <iamvictorcanto@gmail.com>
2021-05-08 12:20:51 -0300

    R1 implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+// source :// https://reactrouter.com/web/guides/quick-start
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </Router>
+
   );
 }
 
