798f9f906d5a27231ae2d81c56ddd7c72c1b8277
Victor Canto <iamvictorcanto@gmail.com>
2021-05-08 14:10:56 -0300

    R2 implemented

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -12,11 +13,20 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.onGetMovies();
+  }
+
+  async onGetMovies() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies,
+    });
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    if (movies.length === 0) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
