2e38607a84c93aea98083f1cd5f284408f8f4f18
Victor Canto <iamvictorcanto@gmail.com>
2021-05-09 00:59:02 -0300

    R5 implemented

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.onGetMovie();
+  }
+
   handleSubmit(updatedMovie) {
+    this.setState(
+      { shouldRedirect: false },
+      async () => {
+        await movieAPI.updateMovie(updatedMovie);
+        this.setState({ shouldRedirect: true });
+      },
+    );
+  }
+
+  async onGetMovie() {
+    this.setState(
+      { status: 'loading' },
+      async () => {
+        const { match: { params: { id } } } = this.props;
+        const movie = await movieAPI.getMovie(id);
+        this.setState({
+          status: '',
+          movie,
+        });
+      },
+    );
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
