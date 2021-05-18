8f9527a08bb641f3cc22a5d6f922d1d4214e578d
Victor Canto <iamvictorcanto@gmail.com>
2021-05-18 09:42:17 -0300

    fix update branch

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ShowAddToCart from '../components/ShowAddToCart';
 
 export default class ShoppingCart extends Component {
   render() {
     return (
       <div>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <ShowAddToCart />
       </div>
     );
   }
