ecc39bc5220187b5f6d2711319d2bc6b555baa86
Victor Canto <iamvictorcanto@gmail.com>
2021-05-26 21:02:51 -0300

    feat: requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils';
+import NotFound from '../components/NotFound';
+
+test('page contains an h2 heading with the text Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+  expect(screen.getByRole('heading', { name: 'Page requested not found Crying emoji' }))
+    .toBeInTheDocument();
+});
+
+test('page shows the image', () => {
+  renderWithRouter(<NotFound />);
+  const img = screen.getAllByRole('img');
+  expect(img[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
