6f9eda5b73a1afcf20f98f4ed25b407dac6910dc
Victor Canto <iamvictorcanto@gmail.com>
2021-06-08 19:57:39 -0300

    feat: requirement 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="user_email"
+            id="user_email"
+            placeholder="Digite seu e-mail"
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="user_password"
+            id="user_password"
+            placeholder="Digite sua senha"
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
