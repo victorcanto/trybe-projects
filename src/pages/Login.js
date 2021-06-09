443f09ac3e73da29174b088653797d5cccec48ea
Victor Canto <iamvictorcanto@gmail.com>
2021-06-08 23:33:17 -0300

    feat: requirement 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validate = this.validate.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validate(email, password) {
+    // source regex: https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+    const re = /\S+@\S+\.\S+/;
+    const minimumCharacters = 6;
+    const emailTest = re.test(email);
+    const passwordTest = password.length >= minimumCharacters;
+
+    return !(emailTest && passwordTest);
+  }
+
   render() {
+    const { email, password } = this.state;
+    const disabled = this.validate(email, password);
     return (
       <div>
-        <form>
+        <form onSubmit={ this.handleSubmit }>
           <input
             data-testid="email-input"
             type="email"
-            name="user_email"
+            name="email"
             id="user_email"
             placeholder="Digite seu e-mail"
+            value={ email }
+            onChange={ this.handleChange }
           />
           <input
             data-testid="password-input"
             type="password"
-            name="user_password"
+            name="password"
             id="user_password"
             placeholder="Digite sua senha"
+            value={ password }
+            onChange={ this.handleChange }
           />
-          <button type="submit">Entrar</button>
+          <Link to="/carteira">
+            <button
+              type="submit"
+              disabled={ disabled }
+            >
+              Entrar
+            </button>
+          </Link>
         </form>
       </div>
     );
