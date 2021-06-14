7420fa356f0d0059dc30d3e3154878ead9baea43
phlopes <phlopes93@gmail.com>
2021-06-14 16:18:03 -0300

    feat: implementacao do botao iniciar jogo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import ScreenGame from './components/ScreenGame';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ ScreenGame } />
     </Switch>
   );
 }
---
+++ src/components/ScreenGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ScreenGame extends React.Component {
+  render() {
+    return (
+      <h1>Game</h1>
+    );
+  }
+}
+
+export default ScreenGame;
