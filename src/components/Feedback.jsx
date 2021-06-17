ec0b52a7600228f975a7c2ffdc9912a67420fcfe
phlopes <phlopes93@gmail.com>
2021-06-17 17:52:43 -0300

    feat: create feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// const lessThanThree = 'Podia ser melhor...';
+// const moreThanThree = 'Mandou bem!';
+// const { player: { assertions } } = JSON.parse(localStorage.getItem('state'))
 
 class Feedback extends Component {
   render() {
     return (
       <div>
-        <h1 data-testid="feedback-text">Tela de Feedback</h1>
+        {/* <h1 data-testid="feedback-text">Tela de Feedback</h1>
+        {assertions < 3 ? moreThanThree : lessThanThree} */}
       </div>
     );
   }
 }
 
-export default Feedback;
+export default connect()(Feedback);
