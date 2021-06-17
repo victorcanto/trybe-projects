bd77014911b97f4691b0c3e0ee088a6ac5e428a7
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-06-17 18:53:36 -0300

    Feat: Requirement 14

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -5,13 +5,15 @@ class Feedback extends Component {
   render() {
     const lessThanThree = <span data-testid="feedback-text">Podia ser melhor...</span>;
     const moreThanThree = <span data-testid="feedback-text">Mandou bem!</span>;
-    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
     const numberAssertions = 3;
     return (
       <div>
         <Header />
         <h1>Tela de Feedback</h1>
         {assertions < numberAssertions ? lessThanThree : moreThanThree}
+        <p data-testid="feedback-total-score">{ score }</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
       </div>
     );
   }
