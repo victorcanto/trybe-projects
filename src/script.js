117d3d9b685fe6b90969e87206b8e97351c4c149
Victor Canto <iamvictorcanto@gmail.com>
2021-06-21 18:26:34 -0300

    feat: requirement 1

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const elTextInput = document.getElementById('text-input');
+const elMemeText = document.getElementById('meme-text');
+
+function handleChangeMemeText(e) {
+   return elMemeText.textContent = e.target.value;
+}
+
+elTextInput.addEventListener('keyup', handleChangeMemeText);
