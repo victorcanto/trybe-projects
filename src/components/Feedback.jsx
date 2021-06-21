c4c918d969d683253dd40e0c770b29f17a0cd15e
Orlando Flores <orlando_flores@id.uff.br>
2021-06-20 21:18:00 -0300

    feat: adiciona estilo para a pagina de feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import { resetQuestions, resetLogin, resetScore } from '../Redux/actions';
+import '../styles/feedback.css';
 
 class Feedback extends Component {
   componentDidMount() {
@@ -21,34 +22,47 @@ class Feedback extends Component {
   }
 
   render() {
-    const lessThanThree = <span data-testid="feedback-text">Podia ser melhor...</span>;
-    const moreThanThree = <span data-testid="feedback-text">Mandou bem!</span>;
+    const lessThanThree = <h1 data-testid="feedback-text">Podia ser melhor...</h1>;
+    const moreThanThree = <h1 data-testid="feedback-text">Mandou bem!</h1>;
     const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
     const numberAssertions = 3;
     return (
       <div>
         <Header />
-        <h1>Tela de Feedback</h1>
-        {assertions < numberAssertions ? lessThanThree : moreThanThree}
-        <p data-testid="feedback-total-score">{ score }</p>
-        <p data-testid="feedback-total-question">{ assertions }</p>
-        <Link to="/">
-          <button
-            onClick={ () => this.resetState() }
-            type="button"
-            data-testid="btn-play-again"
-          >
-            Jogar Novamente
-          </button>
-        </Link>
-        <Link to="/ranking">
-          <button
-            type="button"
-            data-testid="btn-ranking"
-          >
-            Ver Ranking
-          </button>
-        </Link>
+        <div className="feedback-container">
+          <div className="feedback-score-container">
+            {assertions < numberAssertions ? lessThanThree : moreThanThree}
+            <p>
+              Pontuação:
+              <span data-testid="feedback-total-score">{ score }</span>
+            </p>
+            <p>
+              Total de acertos:
+              <span data-testid="feedback-total-question">{ assertions }</span>
+            </p>
+          </div>
+          <div className="feedback-buttons-container">
+            <Link to="/">
+              <button
+                className="feedback_btn_next"
+                onClick={ () => this.resetState() }
+                type="button"
+                data-testid="btn-play-again"
+              >
+                Jogar Novamente
+              </button>
+            </Link>
+            <Link to="/ranking">
+              <button
+                className="feedback_btn_next"
+                type="button"
+                data-testid="btn-ranking"
+              >
+                Ver Ranking
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
