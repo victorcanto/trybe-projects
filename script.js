34b2da1e8216a1f29a8cdfe47c6caf503378df7c
Victor Canto <iamvictorcanto@gmail.com>
2021-06-22 20:46:24 -0300

    fix: lint errors

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const elImageInput = document.getElementById('meme-insert');
 const elMemeImage = document.getElementById('meme-image');
 const elImageContainer = document.getElementById('meme-image-container');
 const elBtns = document.querySelectorAll('.border-styles button');
-const elMemesReady =document.querySelectorAll('.memes-ready img');
+const elMemesReady = document.querySelectorAll('.memes-ready img');
 const elForm = document.getElementById('form');
-
 
 function handleChangeMemeText(e) {
   elMemeText.textContent = e.target.value;
@@ -17,8 +16,8 @@ elTextInput.addEventListener('input', handleChangeMemeText);
 // source: https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/
 function readImage() {
   if (this.files && this.files[0]) {
-    var file = new FileReader();
-    file.onload = function (e) {
+    const file = new FileReader();
+    file.onload = (e) => {
       elMemeImage.src = e.target.result;
     };
     file.readAsDataURL(this.files[0]);
@@ -33,7 +32,7 @@ function handleBtns({ target: { id } }) {
 
 elBtns.forEach((btn) => {
   btn.addEventListener('click', handleBtns);
-})
+});
 
 function handleMemesReady(e) {
   elMemeImage.src = e.target.src;
@@ -41,14 +40,14 @@ function handleMemesReady(e) {
 
 elMemesReady.forEach((meme) => {
   meme.addEventListener('click', handleMemesReady);
-})
+});
 
 function handleSubmit() {
   elForm.addEventListener('submit', (e) => {
     e.preventDefault();
-  })
+  });
 }
 
 window.onload = () => {
-  handleSubmit()
-}
+  handleSubmit();
+};
