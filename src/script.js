96464d7f528d08af2033086040cf67c8861c0728
Victor Canto <iamvictorcanto@gmail.com>
2021-06-22 15:35:23 -0300

    feat: requirement 16

--- src/script.js
+++ src/script.js
@@ -3,21 +3,37 @@ const elTextLetter = document.getElementById('carta-texto');
 const elGenerateLetter = document.getElementById('carta-gerada');
 const elCreateLetter = document.getElementById('criar-carta');
 
-const msg = "Por favor, digite o conteúdo da carta.";
+const arrayStyles = ['newspaper', 'magazine1', 'magazine2'];
+const arraySizes = ['big', 'reallybig'];
+const arrayRotation = ['rotateleft', 'rotateright'];
+const arraySkew = ['skewleft', 'skewright'];
 
 elForm.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
+function randomClasses() {
+  const elLetter = document.querySelector('#carta-gerada span');
+
+  const randomStyle = arrayStyles[Math.floor(Math.random() * arrayStyles.length)];
+  const randomSize = arraySizes[Math.floor(Math.random() * arraySizes.length)];
+  const randomRotation = arrayRotation[Math.floor(Math.random() * arrayRotation.length)];
+  const randomSkew = arraySkew[Math.floor(Math.random() * arraySkew.length)];
+
+  elLetter.classList.add(randomStyle, randomSize, randomRotation, randomSkew);
+}
+
 function createLetters() {
   const { value } = elTextLetter;
   if (value.length === 0) {
-    elGenerateLetter.innerText = msg;
+    elGenerateLetter.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
     elGenerateLetter.innerText = '';
     const letter = document.createElement('span');
     letter.textContent = value;
+    letter.id = 'letter';
     elGenerateLetter.appendChild(letter);
+    randomClasses();
   }
 }
 
