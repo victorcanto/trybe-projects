d9e2932c063913be8bbb437ca8acc37be9523253
Victor Canto <iamvictorcanto@gmail.com>
2021-06-28 16:06:16 -0300

    feat: remove selected filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,15 +2,29 @@ import React, { useContext } from 'react';
 import AppContext from '../contexts/AppContext';
 
 function Filter() {
-  const { filters: { filterByNumericValues } } = useContext(AppContext);
+  const { filters: { filterByNumericValues }, states: { data },
+    sets: { setFilter, setMutableData } } = useContext(AppContext);
+
+  function removeFilter(e) {
+    const columnName = e.target.parentNode.textContent.split('-')[0];
+    const updatedFilter = filterByNumericValues
+      .filter(({ column }) => column !== columnName);
+
+    setFilter(updatedFilter);
+  }
+
   function renderFilters() {
+    if (!filterByNumericValues.length) {
+      setMutableData(data);
+    }
     return filterByNumericValues
       .map(({ column, comparison, value }, index) => (
         <div
           key={ index }
           data-testid="filter"
         >
-          {`Fitro ${index}: ${column}-${comparison}-${value}`}
+          {`${column}-${comparison}-${value}`}
+          <button type="button" onClick={ removeFilter }>X</button>
         </div>));
   }
   return (
