acc482a83604f70f4cd9b918e585c68b830d555c
Victor Canto <iamvictorcanto@gmail.com>
2021-06-30 03:04:51 -0300

    feat: updates MainCard component

--- src/components/MainScreen/MainCard.jsx
+++ src/components/MainScreen/MainCard.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 
 function MainCard(props) {
-  const { name, thumb } = props;
+  const { id, name, thumb } = props;
+  const { pathname } = useLocation();
   return (
-    <div>
-      <img src={ thumb } alt={ name } width="100px" />
-      <p>{name}</p>
-    </div>
+    <Link to={ `${pathname}/${id}` }>
+      <div>
+        <img src={ thumb } alt={ name } width="100px" />
+        <p>{name}</p>
+      </div>
+    </Link>
+
   );
 }
 
 export default MainCard;
 
 MainCard.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   thumb: PropTypes.string.isRequired,
 };
