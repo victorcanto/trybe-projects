3fcc9513d120d5e75f8df396c7bed19159d8d906
Victor Canto <iamvictorcanto@gmail.com>
2021-06-30 02:55:41 -0300

    feat: add routes to test components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
+import MealScreen from './components/MainScreen/MealScreen';
+import DrinkScreen from './components/MainScreen/DrinkScreen';
+import DetailScreen from './components/DetailScreen/DetailScreen';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div className="app-container">
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/comidas/" component={ MealScreen } />
+        <Route exact path="/bebidas/" component={ DrinkScreen } />
+        <Route path="/comidas/:id" component={ DetailScreen } />
+        <Route path="/bebidas/:id" component={ DetailScreen } />
+      </Switch>
     </div>
+
   );
 }
 
