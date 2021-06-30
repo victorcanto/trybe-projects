a45067213bf37ebb8bc1dcf5ca81fe89a71559f4
Victor Canto <iamvictorcanto@gmail.com>
2021-06-29 22:54:28 -0300

    feat: create main screen components

---
+++ src/components/MainScreen/DrinkScreen.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { fetchRecipes, fetchCategories } from '../../services/MainScreenAPI';
+import MainCard from './MainCard';
+
+const DRINK_DOMAIN = 'thecocktaildb';
+const drinks = 'drinks';
+const QTD_RECIPES = 12;
+const QTD_CATEGORIES = 5;
+
+function DrinkScreen() {
+  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  async function requestRecipes() {
+    const res = await fetchRecipes(drinks, DRINK_DOMAIN, QTD_RECIPES);
+    setData(res);
+  }
+
+  async function requestCategories() {
+    const res = await fetchCategories(drinks, DRINK_DOMAIN, QTD_CATEGORIES);
+    setCategories(res);
+  }
+
+  useEffect(() => {
+    requestRecipes();
+    requestCategories();
+  }, []);
+
+  function renderCards() {
+    if (data.length) {
+      return data.map(({ strDrink, strDrinkThumb }, index) => (<MainCard
+        key={ index }
+        name={ strDrink }
+        thumb={ strDrinkThumb }
+      />));
+    }
+  }
+
+  function renderFilters() {
+    if (categories.length) {
+      return categories.map(({ strCategory }) => (
+        <button
+          type="button"
+          key={ strCategory }
+          data-testid={ `${strCategory}-category-filter` }
+        >
+          {strCategory}
+        </button>));
+    }
+  }
+
+  console.log(categories);
+
+  return (
+    <div>
+      {renderFilters()}
+      {renderCards()}
+    </div>
+  );
+}
+
+export default DrinkScreen;
