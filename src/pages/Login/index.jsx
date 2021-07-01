66e8a33524a58c2cfe4b83fa05264500b02a93eb
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-07-01 11:01:21 -0300

    feat: Login page added with validate function

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
+
+import styles from './login.module.scss';
 
 function Login() {
-  return <div>Login</div>;
+  const history = useHistory();
+  const [userEmail, setUserEmail] = useState('');
+  const [userPwd, setUserPwd] = useState('');
+  const [isValidated, setIsValidated] = useState(false);
+
+  const handleChange = ({ target: { value } }, setState) => {
+    setState(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Cliclado');
+    // history.push('/');
+  };
+
+  useEffect(() => {
+    const regex = /\S+@\S+\.\S+/;
+    const minimumLength = 6;
+    const isEmailValid = regex.test(userEmail);
+    const isPwdValid = userPwd.length >= minimumLength;
+
+    setIsValidated(isEmailValid && isPwdValid);
+  }, [userEmail, userPwd]);
+
+  return (
+    <main className={ styles.container }>
+      {/* <Logo /> */}
+
+      <form className={ styles.loginSection }>
+        <label htmlFor="email-input" className={ styles.userInput }>
+          <input
+            id="email-input"
+            data-testid="email-input"
+            value={ userEmail }
+            onChange={ (event) => handleChange(event, setUserEmail) }
+            type="text"
+            placeholder="E-mail"
+          />
+        </label>
+
+        <label htmlFor="password-input" className={ styles.userInput }>
+          <input
+            id="password-input"
+            data-testid="password-input"
+            value={ userPwd }
+            onChange={ (event) => handleChange(event, setUserPwd) }
+            type="text"
+            placeholder="Password"
+          />
+        </label>
+
+        <button
+          data-testid="login-submit-btn"
+          type="submit"
+          onClick={ handleSubmit }
+          disabled={ !isValidated }
+        >
+          <span>ENTRAR</span>
+          {' '}
+          <FiArrowRight size="1.5rem" />
+        </button>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
