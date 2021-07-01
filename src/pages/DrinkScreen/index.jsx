143ff2a91985b794a6be911809c771b22e8a1856
Victor Canto <iamvictorcanto@gmail.com>
2021-07-01 08:38:40 -0300

    fix: remove merge description

--- src/pages/DrinkScreen/index.jsx
+++ src/pages/DrinkScreen/index.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import useCategories from '../../hooks/useCategories';
 import useRecipes from '../../hooks/useRecipes';
-<<<<<<< HEAD:src/pages/MainScreen/DrinkScreen.jsx
 import Footer from '../../components/Footer';
-=======
 import MainCard from '../../components/MainCard';
->>>>>>> main-group-13:src/pages/DrinkScreen/index.jsx
 import { fetchRecipesByCategory } from '../../services/MainScreenAPI';
 import Header from '../../components/Header/Header';
 
