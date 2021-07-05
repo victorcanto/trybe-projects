3e19ddadd08e7a51c353a9e10094079f908d2a40
Samuel Melo <samuelmmjunior@gmail.com>
2021-07-05 16:13:00 -0300

    fix: testdo footer

--- src/pages/Explorer/Food/Origin/index.jsx
+++ src/pages/Explorer/Food/Origin/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
-import Footer from '../../../../components/footer';
-
-function FoodOrigins() {
-=======
 import Footer from '../../../../components/Footer';
 
-function ExploreOrigins() {
->>>>>>> ea9fdf9d348650c5df837a49ae8b8123a6a45dd0
+function FoodOrigins() {
   return (
     <div>
       <Footer />
@@ -15,8 +9,4 @@ function ExploreOrigins() {
   );
 }
 
-<<<<<<< HEAD
 export default FoodOrigins;
-=======
-export default ExploreOrigins;
->>>>>>> ea9fdf9d348650c5df837a49ae8b8123a6a45dd0
