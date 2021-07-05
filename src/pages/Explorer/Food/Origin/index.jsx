484e34364350d8f6569a813aa4b72477cf32b870
Samuel Melo <samuelmmjunior@gmail.com>
2021-07-05 15:29:30 -0300

    fix: testes do footer

---
+++ src/pages/Explorer/Food/Origin/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../../../../components/footer';
+
+function FoodOrigins() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default FoodOrigins;
