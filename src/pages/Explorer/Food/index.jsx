d35268eee48aa66c9b86aded7d6f7124e35bb8fb
Samuel Melo <samuelmmjunior@gmail.com>
2021-07-07 17:20:41 -0300

    tela de explorar surprenda-me

--- src/pages/Explorer/Food/index.jsx
+++ src/pages/Explorer/Food/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../../../components/Header/Header';
 import Footer from '../../../components/Footer';
+import { fetchRandomApiFood } from '../../../services/MainScreenAPI';
 
 function ExploreFoods() {
+  const [idMeal, setIdMeal] = useState('');
+
+  useEffect(() => {
+    const getId = async () => {
+      const { idMeal: id } = await fetchRandomApiFood();
+      setIdMeal(id);
+    };
+    getId();
+  }, []);
   return (
     <div>
+      <Header title="Explorar Comidas" icon="false" />
+      <a href="/explorar/comidas/ingredientes">
+        <h2 data-testid="explore-by-ingredient">Por Ingredientes</h2>
+      </a>
+      <a href="/explorar/comidas/area">
+        <h2 data-testid="explore-by-area">Por Local de Origem</h2>
+      </a>
+      <a href={ `/explorar/comidas/${idMeal}` }>
+        <h2 data-testid="explore-surprise">Me Surpreenda!</h2>
+      </a>
       <Footer />
     </div>
   );
--- src/pages/Explorer/index.jsx
+++ src/pages/Explorer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import Footer from '../../components/Footer';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/index';
 
 function Explorer() {
   return (
     <div>
+      <Header title="Explorar" icon="false" />
+      <a href="/explorar/comidas">
+        <h2 data-testid="explore-food">Explorarar Comidas</h2>
+      </a>
+      <a href="/explorar/bebidas">
+        <h2 data-testid="explore-drinks">Explorarar Bebidas</h2>
+      </a>
       <Footer />
     </div>
   );
