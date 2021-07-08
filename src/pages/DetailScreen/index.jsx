b0ae1c3cdf17437fdd6aee04efe135c608e866c6
Victor Canto <iamvictorcanto@gmail.com>
2021-07-08 19:00:39 -0300

    feat: DetailScreen

--- src/pages/DetailScreen/index.jsx
+++ src/pages/DetailScreen/index.jsx
@@ -1,10 +1,9 @@
-import React, { } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
-import useRecipeDetails from '../../hooks/useRecipeDetails';
 import '../../styles/global.scss';
 import c from './constants';
-import useRecipes from '../../hooks/useRecipes';
-// import Context from '../../context/DetailScreen/DetailContext';
+import Loading from '../../components/Loading';
+import DetailContext from '../../context/DetailScreen/DetailContext';
 import BasicInfo from '../../components/RecipeDetails/BasicInfo';
 import InteractiveButtons from '../../components/RecipeDetails/InteractiveButtons';
 import Ingredients from '../../components/RecipeDetails/Ingredients';
@@ -14,9 +13,16 @@ import Recommendations from '../../components/RecipeDetails/Recommendations';
 import StartRecipe from '../../components/RecipeDetails/StartRecipe';
 
 function DetailScreen() {
+  const {
+    setInfoDetails,
+    setInfoRecommended,
+    recipeDetails,
+    isLoading,
+  } = useContext(DetailContext);
+
   const { id } = useParams();
   const { pathname } = useLocation();
-  const split = pathname.split('/')[1];
+  const foodOrDrink = pathname.split('/')[1];
   const data = {
     comidas: {
       key: c.meals,
@@ -42,40 +48,82 @@ function DetailScreen() {
 
   const API_INFO_DETAILS = {
     id,
-    key: data[split].key,
-    domain: data[split].domain,
-  };
-
-  const type = {
-    name: data[split].name,
-    category: data[split].category,
-    nameRecommend: data[split].nameRecommend,
-    categoryRecommend: data[split].categoryRecommend,
+    key: data[foodOrDrink].key,
+    domain: data[foodOrDrink].domain,
   };
 
   const API_INFO_RECOMMENDED = {
-    key: data[split].keyRecommend,
-    domain: data[split].domainRecommend,
+    key: data[foodOrDrink].keyRecommend,
+    domain: data[foodOrDrink].domainRecommend,
     qtdR: 6,
   };
 
-  const recipeDetails = useRecipeDetails(API_INFO_DETAILS);
-  const recommendedRecipes = useRecipes(API_INFO_RECOMMENDED);
+  const type = {
+    name: data[foodOrDrink].name,
+    category: data[foodOrDrink].category,
+    nameRecommend: data[foodOrDrink].nameRecommend,
+    categoryRecommend: data[foodOrDrink].categoryRecommend,
+  };
+
+  // const recipeDetails = useRecipeDetails(API_INFO_DETAILS);
+  // const recommendedRecipes = useRecipes(API_INFO_RECOMMENDED);
+
+  useEffect(() => {
+    setInfoDetails(API_INFO_DETAILS);
+    setInfoRecommended(API_INFO_RECOMMENDED);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function handleStorage() {
+    let duplicated = false;
+    let savedRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+    let alcoholicOrNot = '';
+    let area = '';
+    if (type.category === 'strAlcoholic') alcoholicOrNot = 'Alcoholic';
+    if (recipeDetails.strArea) area = recipeDetails.strArea;
+
+    const favoriteRecipe = {
+      id: recipeDetails[`id${type.name}`],
+      type: foodOrDrink.split('s')[0],
+      area,
+      category: recipeDetails.strCategory,
+      alcoholicOrNot,
+      name: recipeDetails[`str${type.name}`],
+      image: recipeDetails[`str${type.name}Thumb`],
+    };
+
+    if (savedRecipes) {
+      savedRecipes = savedRecipes
+        .filter(({ id: recipeId }) => {
+          const isNotDuplicated = recipeId !== recipeDetails[`id${type.name}`];
+          if (!isNotDuplicated) duplicated = true;
+          return isNotDuplicated;
+        });
+      if (duplicated) {
+        return localStorage
+          .setItem('favoriteRecipes', JSON.stringify(savedRecipes));
+      }
+      return localStorage
+        .setItem('favoriteRecipes', JSON.stringify([...savedRecipes, favoriteRecipe]));
+    }
+
+    localStorage
+      .setItem('favoriteRecipes', JSON.stringify([favoriteRecipe]));
+  }
 
   function renderDetails() {
     return (
       <>
         <BasicInfo
-          recipeDetails={ recipeDetails }
           name={ type.name }
           category={ type.category }
         />
-        <InteractiveButtons />
-        <Ingredients recipeDetails={ recipeDetails } />
-        <Instructions recipeDetails={ recipeDetails } />
-        <VideoRecipe recipeDetails={ recipeDetails } name={ type.name } />
+        <InteractiveButtons handleStorage={ handleStorage } id={ id } />
+        <Ingredients />
+        <Instructions name={ type.name } />
+        <VideoRecipe name={ type.name } />
         <Recommendations
-          recipes={ recommendedRecipes }
           name={ type.nameRecommend }
           category={ type.categoryRecommend }
         />
@@ -86,7 +134,7 @@ function DetailScreen() {
 
   return (
     <div>
-      {renderDetails()}
+      {isLoading ? <Loading /> : renderDetails()}
     </div>
 
   );
