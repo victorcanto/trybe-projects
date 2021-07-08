8f4b4fe2daf372e45d3832454e1689e1383a01cf
Victor Canto <iamvictorcanto@gmail.com>
2021-07-08 18:56:01 -0300

    feat: update Hooks

--- src/hooks/useRecipeDetails.js
+++ src/hooks/useRecipeDetails.js
@@ -3,16 +3,18 @@ import fetchRecipeById from '../services/DetailScreenAPI';
 
 function useRecipeDetails({ id, key, domain }) {
   const [recipeDetails, setRecipeDetails] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function requestRecipe() {
       const res = await fetchRecipeById(id, key, domain);
       setRecipeDetails(res[0]);
+      setIsFetching(false);
     }
     requestRecipe();
   }, [domain, id, key]);
 
-  return recipeDetails;
+  return [recipeDetails, isFetching];
 }
 
 export default useRecipeDetails;
--- src/hooks/useRecipes.js
+++ src/hooks/useRecipes.js
@@ -3,16 +3,18 @@ import { fetchRecipes } from '../services/MainScreenAPI';
 
 function useRecipes({ key, domain, qtdR }) {
   const [recipes, setRecipes] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function requestRecipes() {
       const res = await fetchRecipes(key, domain, qtdR);
       setRecipes(res);
+      setIsFetching(false);
     }
     requestRecipes();
   }, [key, domain, qtdR]);
 
-  return recipes;
+  return [recipes, isFetching];
 }
 
 export default useRecipes;
