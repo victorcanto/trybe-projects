cff6432de996f96aae86116d5e62f3f611fd7886
Victor Canto <iamvictorcanto@gmail.com>
2021-07-16 20:15:02 -0300

    fix: Header in Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 
 function Profile() {
   function getEmail() {
@@ -10,6 +11,7 @@ function Profile() {
 
   return (
     <div>
+      <Header title="Perfil" icon="false" />
       <p data-testid="profile-email">{getEmail()}</p>
       <div>
         <Link to="/receitas-feitas">
