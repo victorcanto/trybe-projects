2386facdc6623e7a61299fb978f878426e728bef
Victor Canto <iamvictorcanto@gmail.com>
2021-07-16 22:07:23 -0300

    feat: requestCategoriesArea

--- src/pages/Explorer/Food/Origin/index.jsx
+++ src/pages/Explorer/Food/Origin/index.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../../../../components/Footer';
 import Header from '../../../../components/Header';
+import { fetchCategoriesByArea } from '../../../../services/recipesApi';
+import useRecipes from '../../../../hooks/useRecipes';
 
 function FoodOrigin() {
+  const [categoriesArea, setCategoriesArea] = useState([]);
+
+  useEffect(() => {
+    const requestCategories = async () => {
+      const res = await fetchCategoriesByArea();
+      setCategoriesArea(res);
+    };
+    requestCategories();
+  }, []);
+
   return (
     <div>
-      {/* <select data-testid="explore-by-area-dropdown">
+      <Header title="Explorar Origem" icon />
+      <select data-testid="explore-by-area-dropdown">
         <option disabled="">Selecione uma opção</option>
         <option data-testid="All-option">All</option>
-        {areas.map((area) => (
-          <option data-testid={ `${area}-option` } key={ area }>
-            {area}
+        {categoriesArea.map(({ strArea }) => (
+          <option data-testid={ `${strArea}-option` } key={ strArea }>
+            {strArea}
           </option>
         ))}
-      </select> */}
-      <Header title="Explorar Origem" icon />
+      </select>
       <Footer />
     </div>
   );
