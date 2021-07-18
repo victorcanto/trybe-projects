f2be39672e0be3b29c531ec5f0d44d192c2428a3
Victor Canto <iamvictorcanto@gmail.com>
2021-07-18 13:08:31 -0300

    feat: FoodOrigin

--- src/pages/Explorer/Food/Origin/index.jsx
+++ src/pages/Explorer/Food/Origin/index.jsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Footer from '../../../../components/Footer';
 import Header from '../../../../components/Header';
-import { fetchCategoriesByArea } from '../../../../services/recipesApi';
-import useRecipes from '../../../../hooks/useRecipes';
+import { fetchCategoriesByArea, filterByArea } from '../../../../services/recipesApi';
+import MainCard from '../../../../components/MainCard';
+import FoodContext from '../../../../context/FoodProvider/FoodContext';
+
+const MEALS_SIZE = 12;
 
 function FoodOrigin() {
+  const { foodRecipes } = useContext(FoodContext);
   const [categoriesArea, setCategoriesArea] = useState([]);
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+  const [value, setValue] = useState('All');
 
   useEffect(() => {
     const requestCategories = async () => {
@@ -15,18 +21,50 @@ function FoodOrigin() {
     requestCategories();
   }, []);
 
+  useEffect(() => {
+    function selectAll() {
+      if (value === 'All') setFilteredRecipes(foodRecipes);
+    }
+    selectAll();
+  });
+
+  function handleSelect({ target }) {
+    setValue(target.value);
+  }
+
+  useEffect(() => {
+    async function requestRecipe() {
+      const res = await filterByArea(value);
+      setFilteredRecipes(res);
+    }
+    requestRecipe();
+  }, [value]);
+
   return (
     <div>
       <Header title="Explorar Origem" icon />
-      <select data-testid="explore-by-area-dropdown">
-        <option disabled="">Selecione uma opção</option>
+      <select
+        data-testid="explore-by-area-dropdown"
+        onChange={ (e) => handleSelect(e) }
+        value={ value }
+      >
         <option data-testid="All-option">All</option>
         {categoriesArea.map(({ strArea }) => (
-          <option data-testid={ `${strArea}-option` } key={ strArea }>
+          <option data-testid={ `${strArea}-option` } key={ strArea } value={ strArea }>
             {strArea}
           </option>
         ))}
       </select>
+      {filteredRecipes && filteredRecipes.slice(0, MEALS_SIZE)
+        .map(({ idMeal, strMeal, strMealThumb }, index) => (
+          <MainCard
+            currentPage="area"
+            key={ index }
+            index={ index }
+            id={ idMeal }
+            name={ strMeal }
+            thumb={ strMealThumb }
+          />))}
       <Footer />
     </div>
   );
