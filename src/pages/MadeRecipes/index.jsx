2d55a79e6c102ef9e42845679d749e526a3db96d
Victor Canto <iamvictorcanto@gmail.com>
2021-07-18 16:34:38 -0300

    feat: MadeRecipes complete

--- src/pages/MadeRecipes/index.jsx
+++ src/pages/MadeRecipes/index.jsx
@@ -2,81 +2,45 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import styles from './madeRecipes.module.scss';
-
-const MADE_RECIPES = [
-  {
-    type: 'comidas',
-    id: '52977',
-    img: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-    category: 'Side',
-    name: 'Corba',
-    date: '12/07/2021',
-    tags: 'Soup',
-  },
-  {
-    type: 'bebidas',
-    id: '15997',
-    img: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-    category: 'Optional alcohol',
-    name: 'GG',
-    date: '12/07/2021',
-    tags: 'null',
-  },
-
-]; // TEMP
+import shareIcon from '../../images/shareIcon.svg';
+import FilterButtons from '../../components/FilterButtons';
 
 function MadeRecipes() {
+  const MADE_RECIPES = JSON.parse(localStorage.getItem('doneRecipes'));
   const [isCopy, setIsCopy] = useState(false);
-  const [filteredRecipes, setFilteredRecipes] = useState(MADE_RECIPES);
+  const [filteredRecipes, setFilteredRecipes] = useState(MADE_RECIPES || []);
 
   function copyToClipBoard({ type, id }) {
-    const path = `${type}/${id}`;
+    const path = `${type}s/${id}`;
     const FIVE_SECONDS = 5000;
     setIsCopy(true);
     setTimeout(() => setIsCopy(false), FIVE_SECONDS);
     return navigator.clipboard.writeText(`http://localhost:3000/${path}`);
   }
 
-  function filterRecipes(filter) {
-    const filtered = MADE_RECIPES.filter(({ type }) => type === filter);
-    setFilteredRecipes(filtered);
-  }
-
-  function handleFilterButtons({ target }) {
-    const content = target.textContent;
-    switch (content) {
-    case 'Food':
-      filterRecipes('comidas');
-      break;
-    case 'Drinks':
-      filterRecipes('bebidas');
-      break;
-    default:
-      setFilteredRecipes(MADE_RECIPES);
-      break;
-    }
-  }
-
   function renderMadeRecipeCards() {
     return filteredRecipes.map((recipe, index) => (
       <div className={ styles.madeRecipesCard } key={ index }>
-        <Link to={ `${recipe.type}/${recipe.id}` }>
+        <Link to={ `${recipe.type}s/${recipe.id}` }>
           <img
             data-testid={ `${index}-horizontal-image` }
-            src={ recipe.img }
+            src={ recipe.image }
             alt={ recipe.name }
           />
         </Link>
         <div>
-          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
-          <Link to={ `${recipe.type}/${recipe.id}` }>
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {recipe.type === 'comida'
+              ? `${recipe.area} - ${recipe.category}` : recipe.alcoholicOrNot}
+          </p>
+          <Link to={ `${recipe.type}s/${recipe.id}` }>
             <h3 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h3>
           </Link>
-
-          <p data-testid={ `${index}-horizontal-done-date` } />
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
         </div>
         <div>
           <button
+            src={ shareIcon }
             type="button"
             className={ styles.shareButton }
             onClick={ () => copyToClipBoard(recipe) }
@@ -86,11 +50,16 @@ function MadeRecipes() {
           </button>
         </div>
 
-        <p
-          data-testid={ `${index}-${recipe.tags}-horizontal-tag` }
-        >
-          {recipe.tags !== 'null' && recipe.tags }
-        </p>
+        <div>
+          {(recipe.tags !== 'null' && recipe.tags) && recipe.tags
+            .map((tag) => (
+              <span
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+                key={ tag }
+              >
+                {tag}
+              </span>))}
+        </div>
       </div>
     ));
   }
@@ -98,29 +67,10 @@ function MadeRecipes() {
   function renderMadeRecipes() {
     return (
       <div className={ styles.madeRecipes }>
-        <div>
-          <button
-            type="button"
-            data-testid="filter-by-all-btn"
-            onClick={ handleFilterButtons }
-          >
-            All
-          </button>
-          <button
-            type="button"
-            data-testid="filter-by-food-btn"
-            onClick={ handleFilterButtons }
-          >
-            Food
-          </button>
-          <button
-            type="button"
-            data-testid="filter-by-drink-btn"
-            onClick={ handleFilterButtons }
-          >
-            Drinks
-          </button>
-        </div>
+        <FilterButtons
+          recipes={ MADE_RECIPES }
+          setFilteredRecipes={ setFilteredRecipes }
+        />
         {isCopy && <span>Link copiado!</span>}
         <div>
           {renderMadeRecipeCards()}
