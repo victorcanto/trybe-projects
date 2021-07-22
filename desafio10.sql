f0814efc26885dd1250289c7c4fa24758fcb383f
Victor Canto <iamvictorcanto@gmail.com>
2021-07-22 18:09:07 -0300

    feat: challenge 10 done

---
+++ desafio10.sql
@@ -0,0 +1,4 @@
+SELECT * FROM northwind.purchase_orders WHERE created_by >= 3 
+ORDER BY created_by DESC, id ASC;
+
+-- source :https://stackoverflow.com/questions/2051162/sql-multiple-column-ordering/2051176
