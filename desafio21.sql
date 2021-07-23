937ba1bcce0f610adfbb27013a98e11acc18f132
Victor Canto <iamvictorcanto@gmail.com>
2021-07-22 22:33:04 -0300

    fix: challenge 21

--- desafio21.sql
+++ desafio21.sql
@@ -12,5 +12,5 @@ INSERT INTO northwind.order_details
 )
 
 VALUES
-(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
 (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
