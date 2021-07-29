3270f72d0d1b144f0f6572c81216a19c2f55d788
Victor Canto <iamvictorcanto@gmail.com>
2021-07-29 17:04:15 -0300

    fix(challenge18): day and month format

--- desafio18.sql
+++ desafio18.sql
@@ -2,16 +2,32 @@
 SELECT
   CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
   CONCAT(
-    DAY(jh.START_DATE),
+    IF(
+      DAY(jh.START_DATE) < 10,
+      CONCAT('0', DAY(jh.START_DATE)),
+      DAY(jh.START_DATE)
+    ),
     '/',
-    MONTH(jh.START_DATE),
+    IF(
+      MONTH(jh.START_DATE) < 10,
+      CONCAT('0', MONTH(jh.START_DATE)),
+      MONTH(jh.START_DATE)
+    ),
     '/',
     YEAR(jh.START_DATE)
   ) AS 'Data de início',
   CONCAT(
-    DAY(jh.END_DATE),
+    IF(
+      DAY(jh.END_DATE) < 10,
+      CONCAT('0', DAY(jh.END_DATE)),
+      DAY(jh.END_DATE)
+    ),
     '/',
-    MONTH(jh.END_DATE),
+    IF(
+      MONTH(jh.END_DATE) < 10,
+      CONCAT('0', MONTH(jh.END_DATE)),
+      MONTH(jh.END_DATE)
+    ),
     '/',
     YEAR(jh.END_DATE)
   ) AS 'Data de rescisão',
