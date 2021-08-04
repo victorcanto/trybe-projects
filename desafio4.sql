8c0bd9a7ca81baa2b519aaadba885021ed6708c6
Victor Canto <iamvictorcanto@gmail.com>
2021-08-04 14:54:34 -0300

    feat: challenge4 done

---
+++ desafio4.sql
@@ -0,0 +1,21 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+  (
+    SELECT
+      name
+    FROM
+      SpotifyClone.artists
+    WHERE
+      artist_id = fo.fk_artist_id
+  ) AS artista,
+  (
+    SELECT
+      COUNT(fo.fk_user_id)
+  ) AS seguidores
+FROM
+  SpotifyClone.following_artists AS fo
+GROUP BY
+  artista
+ORDER BY
+  seguidores DESC,
+  artista;
