9d70f428ae29734ed11b5d20ec7292a2147276b5
Victor Canto <iamvictorcanto@gmail.com>
2021-08-05 00:27:25 -0300

    fix(challenge10): change logic to receive user id as parameter

--- desafio10.sql
+++ desafio10.sql
@@ -1,17 +1,16 @@
 DELIMITER $$ 
 
-CREATE FUNCTION quantidade_musicas_no_historico(id VARCHAR(50)) 
+CREATE FUNCTION quantidade_musicas_no_historico(id INT) 
 RETURNS INT READS SQL DATA 
 BEGIN 
 DECLARE qtd_songs INT;
 SELECT
-  COUNT(re.fk_song_id)
+  COUNT(fk_song_id)
 FROM
   SpotifyClone.reproduction_history AS re
-  INNER JOIN SpotifyClone.users AS us ON re.fk_user_id = us.user_id
 WHERE
-  us.name = id INTO qtd_songs;
+  re.fk_user_id = id INTO qtd_songs;
 RETURN qtd_songs;
 END $$ 
 
-DELIMITER ;
+DELIMITER;
