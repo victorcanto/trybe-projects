2bb436954017854276ff3b98a3d5ccce3f0edaff
Victor Canto <iamvictorcanto@gmail.com>
2021-08-05 01:00:09 -0300

    test: challenge10

--- desafio10.sql
+++ desafio10.sql
@@ -1,16 +1,14 @@
 DELIMITER $$ 
-
 CREATE FUNCTION quantidade_musicas_no_historico(id INT) 
 RETURNS INT READS SQL DATA 
 BEGIN 
 DECLARE qtd_songs INT;
 SELECT
-  COUNT(fk_song_id)
+  COUNT(*)
 FROM
-  SpotifyClone.reproduction_history AS re
+  SpotifyClone.reproduction_history
 WHERE
-  re.fk_user_id = id INTO qtd_songs;
+  fk_user_id = id INTO qtd_songs;
 RETURN qtd_songs;
 END $$ 
-
-DELIMITER;
+DELIMITER ;
