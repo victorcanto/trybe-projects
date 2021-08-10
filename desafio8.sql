f86f419d88d2466f986f4145a0db4804b65edfc1
Victor Canto <iamvictorcanto@gmail.com>
2021-08-10 14:34:13 -0300

    feat: challenge8 done

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.following_artists WHERE fk_user_id = OLD.user_id;
+  DELETE FROM SpotifyClone.reproduction_history WHERE fk_user_id = OLD.user_id;
+END $$
+
+DELIMITER ;
