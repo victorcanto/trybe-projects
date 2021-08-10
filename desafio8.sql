54d4f13eb68a313c9dc3b2c216627d45d1a1e7a4
Victor Canto <iamvictorcanto@gmail.com>
2021-08-10 14:44:29 -0300

    fix: codeclimate tabs found

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.following_artists WHERE fk_user_id = OLD.user_id;
-  DELETE FROM SpotifyClone.reproduction_history WHERE fk_user_id = OLD.user_id;
+DELETE FROM SpotifyClone.following_artists WHERE fk_user_id = OLD.user_id;
+DELETE FROM SpotifyClone.reproduction_history WHERE fk_user_id = OLD.user_id;
 END $$
 
 DELIMITER ;
