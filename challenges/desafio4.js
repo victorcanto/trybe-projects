a48c9c9d53d3a1612497879f458e2749a8eeb5ff
Victor Canto <iamvictorcanto@gmail.com>
2021-08-17 02:16:12 -0300

    feat: challenge4 done

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+// Crie uma query que atribua a data corrente ao campo ultimaModificacao no sanduíche Big Mac. Para a data corrente faça uso do tipo Date.
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: {
+    ultimaModificacao: { $type: "date" },
+  } },
+);
+
+// Crie uma query que retorne o nome de todos os documentos em que o campo ultimaModificacao existe.
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 },
+);
