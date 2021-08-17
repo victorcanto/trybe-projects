948d97eebb2c373d876f7ab806301e9c7f3cb3c4
Victor Canto <iamvictorcanto@gmail.com>
2021-08-17 02:16:21 -0300

    feat: challenge6 done

---
+++ challenges/desafio6.js
@@ -0,0 +1,13 @@
+// Crie uma query que faça a inclusão de bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo.
+db.produtos.updateMany(
+  { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+  { $push: {
+    ingredientes: "bacon",
+  } },
+);
+
+// Crie uma query que retorne o nome e ingredientes de todos os documentos.
+db.produtos.find(
+  {},
+  { nome: 1, ingredientes: 1, _id: 0 },
+);
