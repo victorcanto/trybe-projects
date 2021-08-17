cb93a70e28ec0ab744b806f4b5c87a19c4d8f32f
Victor Canto <iamvictorcanto@gmail.com>
2021-08-17 02:16:50 -0300

    feat: challenge11 done

---
+++ challenges/desafio11.js
@@ -0,0 +1,16 @@
+// Crie uma query que faça tanto a inserção dos elementos combo e tasty no array tags de todos os sanduíches quanto a ordenação dos elementos de tags em ordem alfabética ascendente.
+db.produtos.updateMany(
+  {},
+  { $push: {
+    tags: {
+      $each: ["combo", "tasty"],
+      $sort: 1,
+    },
+  } },
+);
+
+// Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.find(
+  {},
+  { nome: 1, tags: 1, _id: 0 },
+);
