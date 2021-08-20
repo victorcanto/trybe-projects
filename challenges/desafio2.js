40924de1289cdb60ee87741c03072eb67d109c29
Victor Canto <iamvictorcanto@gmail.com>
2021-08-20 10:29:45 -0300

    feat: challenge 2 done

---
+++ challenges/desafio2.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: {
+        $exists: true,
+        $all: ["English", "Spanish"],
+      },
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+]);
