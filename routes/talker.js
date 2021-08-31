894a0bb09635e0593789823be97e6d314329c419
Victor Canto <iamvictorcanto@gmail.com>
2021-08-31 17:51:32 -0300

    feat: req 2 done

--- routes/talker.js
+++ routes/talker.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 const { getTalkers } = require('../services/readFile');
 
+async function getTalkerById(id) {
+  const talkers = await getTalkers();
+  const talkerFound = await talkers.find((t) => t.id === parseInt(id, 10));
+  return talkerFound;
+}
+
 // 1 - Crie o endpoint GET /talker
 
 router.get('/', async (_req, res) => {
@@ -12,6 +18,21 @@ router.get('/', async (_req, res) => {
   if (!talkers) return res.status(200).send([]);
 
   return res.status(200).json(talkers);
+});
+
+// 2 - Crie o endpoint GET /talker/:id
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerFound = await getTalkerById(id);
+
+  if (!talkerFound) {
+    return res
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(talkerFound);
 });
 
 module.exports = router;
