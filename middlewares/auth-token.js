fc16f2c57dc503e856f433655459f81b8ddb42d9
Victor Canto <iamvictorcanto@gmail.com>
2021-09-01 16:32:57 -0300

    feat(req 4): create authToken middleware

---
+++ middlewares/auth-token.js
@@ -0,0 +1,19 @@
+function authToken(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+
+  if (authorization.length !== 16) {
+    return res.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+
+  next();
+}
+
+module.exports = { authToken };
