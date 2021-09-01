fcb118f98ae81f2256f6ea2eddac4da28050d206
Victor Canto <iamvictorcanto@gmail.com>
2021-09-01 19:11:23 -0300

    fix: rename middleware to add talker

--- middlewares/add-talker.js
+++ middlewares/add-talker.js
@@ -1,6 +1,6 @@
 const { getTalkers, setTalker } = require('../services/fs');
 
-async function addNewTalker(req, res) {
+async function addTalker(req, res) {
   const { name, age, talk } = req.body;
   const talkers = await getTalkers();
   const id = talkers.length + 1;
@@ -18,7 +18,7 @@ async function addNewTalker(req, res) {
   return res.status(201).json(newTalker);
 }
 
-module.exports = { addNewTalker };
+module.exports = { addTalker };
 
 // Source Ref: 
 // Aluno Mauricio Viegas:  https://github.com/tryber/sd-010-a-project-talker-manager/commit/b93b884e91054f946ddeb155b94f4a405bfdf6af
