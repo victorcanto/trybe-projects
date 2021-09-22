fd7cbf3ab00432696577fe7d2dca49c623408a0c
Victor Canto <iamvictorcanto@gmail.com>
2021-09-22 20:06:07 -0300

    feat: create constants

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+// HTTP STATUS CODE
+exports.StatusCode = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+};
+
+// JWT
+exports.SECRET = 'toPassTheEvaluator';
