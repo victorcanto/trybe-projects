e8ae3b06289b2fb8557b9806cbe45c2168342242
Victor Canto <iamvictorcanto@gmail.com>
2021-10-15 17:14:47 -0300

    feat: server

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const {
+  userRouter,
+  loginRouter,
+  categoriesRouter,
+  postRouter,
+} = require('./routes');
+
+const { ErrorMiddleware } = require('./middlewares');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use('/user', userRouter);
+app.use('/login', loginRouter);
+app.use('/categories', categoriesRouter);
+app.use('/post', postRouter);
+
+app.use(ErrorMiddleware);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
