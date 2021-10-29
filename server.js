55763bd092e6c3603c6dd34e70badd1634d89c47
Victor Canto <iamvictorcanto@gmail.com>
2021-10-29 12:49:28 -0300

    feat: server

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+};
+
+const io = require('socket.io')(httpServer, corsOptions);
+require('./sockets/chatSocket')(io);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static(path.join(__dirname, './public')));
+
+const ChatController = require('./controllers/ChatController');
+
+app.get('/', ChatController.getAllMessages);
+
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
