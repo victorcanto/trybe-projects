83e04b91d7129342c32f8372d5a713d7b7478e58
Victor Canto <iamvictorcanto@gmail.com>
2021-11-10 22:29:32 -0300

    feat(req1): routes to create user and login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+const routes = require('./routes');
+
+app.use(routes);
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
