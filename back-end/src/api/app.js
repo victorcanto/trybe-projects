065f02774b729eb02301b798d5658adcc7618eb6
Victor Canto <iamvictorcanto@gmail.com>
2021-11-12 00:27:14 -0300

    feat(req0): add cors to the backend app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 const routes = require('./routes');
 
 app.use(routes);
