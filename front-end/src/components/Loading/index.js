14d149f02cc785faef70aac401aa37bd95461337
Victor Canto <iamvictorcanto@gmail.com>
2021-11-12 00:13:52 -0300

    feat(req0): Loading component

---
+++ front-end/src/components/Loading/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledLoading = styled.div``;
+
+const Loading = () => <StyledLoading>Carregando...</StyledLoading>;
+
+export default Loading;
