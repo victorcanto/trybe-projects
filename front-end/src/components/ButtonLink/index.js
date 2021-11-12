3ab162e4ccdd49fd850646a6811746bce6fbd4bb
Victor Canto <iamvictorcanto@gmail.com>
2021-11-12 20:25:14 -0300

    fix: adiciona o resto das propriedades no StyledButtonLink

--- front-end/src/components/ButtonLink/index.js
+++ front-end/src/components/ButtonLink/index.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import StyledButtonLink from './styles';
 
-const ButtonLink = ({ children, url }) => (
-  <StyledButtonLink data-testid="common_login__button-register">
+const ButtonLink = ({ children, url, ...props }) => (
+  <StyledButtonLink { ...props }>
     <Link to={ url }>
       {children}
     </Link>
