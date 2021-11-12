5b97412793e80d1824b41de66aba2388846c5f71
Victor Canto <iamvictorcanto@gmail.com>
2021-11-11 23:39:22 -0300

    feat(req0): add constant file for front-end testing

---
+++ front-end/src/tests/constants/index.js
@@ -0,0 +1,16 @@
+// INPUTS
+export const VALID_EMAIL = 'example@gmail.com';
+export const VALID_PASS = '123456';
+export const INVALID_EMAIL = 'example@com';
+export const INVALID_PASS = '12345';
+export const EMAIL_NOT_EXISTS = 'notexists@gmail.com';
+export const PASS_NOT_EXISTS = '123456';
+
+// ERRORS
+export const ERR_REQUIRED_EMAIL = '"email" is required';
+export const ERR_REQUIRED_PASS = '"password" is required';
+export const ERR_INVALID_FIELDS = 'Invalid fields';
+export const ERR_EMPTY_EMAIL = '"email" is not allowed to be empty';
+export const ERR_EMPTY_PASS = '"password" is not allowed to be empty';
+export const ERR_INVALID_EMAIL = '"email" must be a valid email';
+export const ERR_INVALID_PASS = '"password" length must be at least 6 characters long';
