51e056cc891fdef30194daa6e1a4c742935c00da
Victor Canto <iamvictorcanto@gmail.com>
2021-11-12 00:15:35 -0300

    styles: StyledForm

---
+++ front-end/src/components/Form/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  gap: 8px;
+
+  input {
+    width: 100%;
+    padding: 4%;
+  }
+
+  button {
+    padding: 4%;
+  }
+`;
+
+export default StyledForm;
