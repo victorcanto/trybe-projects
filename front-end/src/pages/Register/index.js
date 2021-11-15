11e620221f15a86220eb5a9a255f0af0d40a62e2
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-11-15 13:36:24 -0300

    feat(req6 a req10): regsiter user page

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,5 +1,162 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router';
+import Button from '../../components/Button';
+import ErrorMsg from '../../components/ErrorMsg';
+import Form from '../../components/Form';
+import useForm from '../../hooks/useForm';
+import StyledRegister from './styles';
+import { requestRegisterUser } from '../../services/api';
 
-const Register = () => <div>Register</div>;
+const NAME_LENGTH = 12;
+const PASSWORD_LENGTH = 6;
+
+const Register = () => {
+  const history = useHistory();
+  const [{ values }, handleChange, handleSubmit] = useForm();
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const inputName = useRef(null);
+  const inputEmail = useRef(null);
+  const inputPassword = useRef(null);
+
+  const clearInputs = () => {
+    inputName.current.value = '';
+    inputEmail.current.value = '';
+    inputPassword.current.value = '';
+  };
+
+  const handleRegister = async () => {
+    clearInputs();
+
+    const result = await requestRegisterUser(values);
+
+    if (result.message) {
+      clearInputs();
+      setErrorMsg(result.message);
+    } else {
+      history.push('/customer/products');
+    }
+  };
+
+  const request = useCallback(
+    async () => {
+      let message = '';
+      let disabled = true;
+
+      if (Object.keys(values).length) {
+        const result = await requestRegisterUser(values);
+        if (result.message !== 'Invalid fields' && !result.token) {
+          message = result.message;
+          disabled = true;
+        } else {
+          message = '';
+          disabled = false;
+        }
+      }
+      setErrorMsg(message);
+      setIsDisabled(disabled);
+    },
+    [values],
+  );
+
+  const isInvalidFields = useCallback(
+    async () => {
+      const emailRegex = /^\S+@\S+\.\S+$/;
+
+      if (values.name
+        && values.name.length < NAME_LENGTH) {
+        return true;
+      }
+
+      if (values.email && !emailRegex.test(values.email)) {
+        return true;
+      }
+
+      if (values.password
+        && values.password.length < PASSWORD_LENGTH) {
+        return true;
+      }
+
+      return false;
+    },
+    [values],
+  );
+
+  useEffect(() => {
+    const checkInvalidFields = async () => {
+      const isInvalid = await isInvalidFields();
+
+      if (isInvalid) {
+        request();
+      }
+
+      if (values.name
+        && values.name.length >= NAME_LENGTH
+        && values.email
+        && values.email.length
+        && values.password
+        && values.password.length >= PASSWORD_LENGTH) {
+        setIsDisabled(false);
+        setErrorMsg('');
+      }
+    };
+
+    checkInvalidFields();
+  },
+  [isInvalidFields, request, values]);
+
+  return (
+    <StyledRegister>
+      <Form onSubmit={ handleSubmit(handleRegister) }>
+        <label htmlFor="name">
+          <input
+            ref={ inputName }
+            data-testid="common_register__input-name"
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Seu nome"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            ref={ inputEmail }
+            data-testid="common_register__input-email"
+            id="email"
+            name="email"
+            type="text"
+            placeholder="email@tryber.com.br"
+            onChange={ handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            ref={ inputPassword }
+            data-testid="common_register__input-password"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="******"
+            onChange={ handleChange }
+          />
+        </label>
+        <Button
+          disabled={ isDisabled }
+          data-testid="common_register__button-register"
+          type="submit"
+        >
+          Cadastrar
+        </Button>
+        {errorMsg && (
+          <ErrorMsg data-testid="common_register__element-invalid_register">
+            {errorMsg}
+          </ErrorMsg>
+        )}
+      </Form>
+    </StyledRegister>
+  );
+};
 
 export default Register;
--- front-end/src/tests/constants/index.js
+++ front-end/src/tests/constants/index.js
@@ -1,8 +1,10 @@
 // INPUTS
 export const VALID_EMAIL = 'zebirita@email.com';
-export const VALID_PASS = '$#zebirita#$';
 export const INVALID_EMAIL = 'zebirita@com';
+export const VALID_PASS = '$#zebirita#$';
 export const INVALID_PASS = '12345';
+export const VALID_NAME = 'Zé Birita da Silva';
+export const INVALID_NAME = 'Zé Birita';
 export const EMAIL_NOT_EXISTS = 'notexists@email.com';
 export const PASS_NOT_EXISTS = '123456';
 
@@ -13,6 +15,13 @@ export const TESTID_LOGIN_BUTTON = 'common_login__button-login';
 export const TESTID_LOGIN_REGISTER = 'common_login__button-register';
 export const TESTID_LOGIN_MSG_ERROR = 'common_login__element-invalid-email';
 
+// DATA-TESTIDS REGISTER
+export const TESTID_REGISTER_NAME = 'common_register__input-name';
+export const TESTID_REGISTER_EMAIL = 'common_register__input-email';
+export const TESTID_REGISTER_PASSWORD = 'common_register__input-password';
+export const TESTID_REGISTER_BUTTON = 'common_register__button-register';
+export const TESTID_REGISTER_MSG_ERROR = 'common_register__element-invalid_register';
+
 // ERRORS
 export const ERR_REQUIRED_EMAIL = '"email" is required';
 export const ERR_REQUIRED_PASS = '"password" is required';
@@ -21,3 +30,5 @@ export const ERR_EMPTY_EMAIL = '"email" is not allowed to be empty';
 export const ERR_EMPTY_PASS = '"password" is not allowed to be empty';
 export const ERR_INVALID_EMAIL = '"email" must be a valid email';
 export const ERR_INVALID_PASS = '"password" length must be at least 6 characters long';
+export const ERR_INVALID_NAME = '"name" length must be at least 12 characters long';
+export const ERR_USER_ALREADY_EXISTS = 'User already exists';
