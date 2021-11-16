aec3be874eb6d0fa5bf52da85080124ab2241beb
Victor Canto <iamvictorcanto@gmail.com>
2021-11-16 18:55:01 -0300

    feat(req22): add error "No sales found"

--- back-end/src/utils/errors.js
+++ back-end/src/utils/errors.js
@@ -9,10 +9,14 @@ const WRONG_PASSWORD = 'Invalid Password';
 // USER
 const USER_EXISTS = 'User already exists';
 
+// SALES
+const NO_SALES_FOUND = 'No sales found';
+
 module.exports = {
   TOKEN_NOT_FOUND,
   INVALID_TOKEN,
   INVALID_FIELDS,
   WRONG_PASSWORD,
   USER_EXISTS,
+  NO_SALES_FOUND,
 };
