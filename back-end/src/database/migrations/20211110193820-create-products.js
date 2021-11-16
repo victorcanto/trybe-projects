9b4f94d93ec333c59bcc6b37a904ff38ae51d8a0
Victor Canto <iamvictorcanto@gmail.com>
2021-11-16 19:00:19 -0300

    fix(seeders): price type

--- back-end/src/database/migrations/20211110193820-create-products.js
+++ back-end/src/database/migrations/20211110193820-create-products.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(10, 2)
       },
       url_image: {
         type: Sequelize.STRING
