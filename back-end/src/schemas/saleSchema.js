8da16db4491d917655d4510144cbf918b9627d81
Victor Canto <iamvictorcanto@gmail.com>
2021-11-18 19:23:20 -0300

    feat(req20): updated saleSchema

--- back-end/src/schemas/saleSchema.js
+++ back-end/src/schemas/saleSchema.js
@@ -1,13 +1,19 @@
 const Joi = require('joi');
 
+const userId = 'user_id';
+const sellerId = 'seller_id';
+const totalPrice = 'total_price';
+const deliveryAddress = 'delivery_address';
+const deliveryNumber = 'delivery_number';
+const saleDate = 'sale_date';
+
 const saleSchema = Joi.object({
-  userId: Joi.number().empty().required(),
-  sellerId: Joi.number().empty().required(),
-  totalPrice: Joi.number().empty().required(),
-  deliveryAddress: Joi.string().empty().required(),
-  deliveryNumber: Joi.string().empty().required(),
-  saleDate: Joi.date().empty().required(),
-  saleStatus: Joi.string().empty().required(),
+  [userId]: Joi.number().empty().required(),
+  [sellerId]: Joi.number().empty().required(),
+  [totalPrice]: Joi.number().empty().required(),
+  [deliveryAddress]: Joi.string().empty().required(),
+  [deliveryNumber]: Joi.string().empty().required(),
+  [saleDate]: Joi.date().empty().required(),
 });
 
 module.exports = { saleSchema };
