c0473be0f4703be30ac90cfae0fe0254010d7e84
Victor Canto <iamvictorcanto@gmail.com>
2021-11-23 03:05:46 -0300

    fix: Orders page

--- front-end/src/pages/Orders/styles.js
+++ front-end/src/pages/Orders/styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-const StyledOrders = styled.div``;
+const StyledOrders = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2vh;
+  margin-top: 5vh;
+`;
 
 export default StyledOrders;
