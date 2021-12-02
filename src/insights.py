aa4480f38b1152911dc84c955cdea95755d31c08
Victor Canto <iamvictorcanto@gmail.com>
2021-12-02 16:29:40 -0300

    feat: Implement the get_min_salary function

--- src/insights.py
+++ src/insights.py
@@ -64,21 +64,12 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = jobs.read(path)
+    salaries = []
+    for job in jobs_list:
+        if job["min_salary"]:
+            salaries.append(int(job["min_salary"]))
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
