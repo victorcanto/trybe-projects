a2897beebe066459dffe18178c0550eba80da933
Victor Canto <iamvictorcanto@gmail.com>
2021-12-02 16:26:46 -0300

    feat: Implement the get_max_salary function

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = jobs.read(path)
+    salaries = []
+    for job in jobs_list:
+        if job["max_salary"]:
+            salaries.append(int(job["max_salary"]))
+    return max(salaries)
 
 
 def get_min_salary(path):
