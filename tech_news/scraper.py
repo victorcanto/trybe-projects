e1a11f3eebc90aac14c76fccd0268ae230e6b93e
Victor Canto <iamvictorcanto@gmail.com>
2022-01-15 10:35:21 -0300

    feat: :sparkles: creates fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code != 200:
+            return None
+        return response.text
+
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
