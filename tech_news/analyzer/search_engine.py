60694f39be2ad330acb99270c76440268f0e578e
Victor Canto <iamvictorcanto@gmail.com>
2022-01-17 16:04:02 -0300

    refactor: :hammer: use case_insensitive_query func

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -13,9 +13,13 @@ def check_date(date):
         raise ValueError("Data inválida")
 
 
+def case_insensitive_query(field, value):
+    return {field: {"$regex": value, "$options": "i"}}
+
+
 # Requisito 6
 def search_by_title(title):
-    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    news = search_news(case_insensitive_query("title", title))
     return transform_news_data(news)
 
 
@@ -28,11 +32,16 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    news = search_news({"sources": {"$regex": source, "$options": "i"}})
+    news = search_news(case_insensitive_query("sources", source))
     return transform_news_data(news)
 
 
 # Requisito 9
 def search_by_category(category):
-    news = search_news({"categories": {"$regex": category, "$options": "i"}})
+    news = search_news(case_insensitive_query("categories", category))
     return transform_news_data(news)
+
+
+# Refs:
+# https://docs.mongodb.com/manual/reference/operator/query/regex/
+# https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime
