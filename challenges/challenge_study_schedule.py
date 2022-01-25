1c20f4b45afdbbc68dcf53b5389fb1f3d287abbb
Victor Canto <iamvictorcanto@gmail.com>
2022-01-25 20:05:14 -0300

    feat: :sparkles: study_schedule done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time:
+        for entry_time, departure_time in permanence_period:
+            if not entry_time or not departure_time:
+                return None
+            if entry_time <= target_time <= departure_time:
+                counter += 1
+        return counter
+    return None
