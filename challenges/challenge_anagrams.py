070fa1109dc2d23f891ece41e9ce1fce6420f6a7
Victor Canto <iamvictorcanto@gmail.com>
2022-01-25 21:40:32 -0300

    feat: :sparkles: is_anagram done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,36 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+def sort_string(string):
+    return "".join(merge_sort(list(string)))
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_ordered = sort_string(first_string)
+    second_string_ordered = sort_string(second_string)
+    return first_string_ordered == second_string_ordered
