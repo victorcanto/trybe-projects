a0c010d97f448c166e5b254f99aab469bfcfa4bc
Victor Canto <iamvictorcanto@gmail.com>
2022-01-25 21:06:52 -0300

    feat: :sparkles: is_palindrome_recursive done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if low_index > high_index:
+        return True
+    return word[low_index] == word[high_index] and is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+    )
