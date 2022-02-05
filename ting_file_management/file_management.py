a6c3b6fabbfe45c8bc885b7a1db0c91f7f163afa
Victor Canto <iamvictorcanto@gmail.com>
2022-02-05 19:46:36 -0300

    feat: :sparkles: txt_importer function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        f = open(path_file, "r")
+        line_list = f.read().split("\n")
+        f.close()
+        return line_list
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
