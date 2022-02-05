d143f0e3d6fd571512b5ba3b0ce66717e49c80fd
Victor Canto <iamvictorcanto@gmail.com>
2022-02-05 19:12:42 -0300

    feat: :sparkles: class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index <= len(self._data):
+            return self._data[index]
+
+        raise IndexError
