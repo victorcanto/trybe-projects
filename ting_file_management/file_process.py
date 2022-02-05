4634f192169d442631fa4bafb3e531408633da3f
Victor Canto <iamvictorcanto@gmail.com>
2022-02-05 20:48:16 -0300

    feat: :sparkles: process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def check_file_already_exists(path_file, instance):
+    for i in range(len(instance)):
+        return instance.search(i)["nome_do_arquivo"] == path_file
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+
+    file_exists = check_file_already_exists(path_file, instance)
+
+    if not file_exists:
+        processed = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content,
+        }
+
+        instance.enqueue(processed)
+        sys.stdout.write(str(processed))
 
 
 def remove(instance):
